458865470ace11009ef0acc9c214bd340e87f337
Pablo Iacopi <pablo.iacopi@etermax.com>
2021-10-18 16:41:47 -0300

    Added animation with color modification to reproduce the allocation problem

--- issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
@@ -2,13 +2,22 @@ package com.airbnb.lottie.issues
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieProperty
 import com.airbnb.lottie.issues.databinding.IssueReproActivityBinding
+import com.airbnb.lottie.model.KeyPath
 
 class IssueReproActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = IssueReproActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // Reproduce any issues here.
+
+        val shirt = KeyPath("Shirt", "Group 5", "Fill 1")
+        val leftArm = KeyPath("LeftArmWave", "LeftArm", "Group 6", "Fill 1")
+        val rightArm = KeyPath("RightArm", "Group 6", "Fill 1")
+
+        binding.animationView.addValueCallback(shirt, LottieProperty.COLOR) { 0xff5a5f }
+        binding.animationView.addValueCallback(leftArm, LottieProperty.COLOR) { 0xff5a5f }
+        binding.animationView.addValueCallback(rightArm, LottieProperty.COLOR) { 0xff5a5f }
     }
 }
