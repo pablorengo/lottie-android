462e98b24aae200c538a38378e46d2a7dcd53401
Pablo Iacopi <pablo.iacopi@etermax.com>
2021-10-19 15:19:09 -0300

    Modify opacity to validate FillContent and BaseStrokeContent alpha color configuration

--- issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
@@ -15,9 +15,15 @@ class IssueReproActivity : AppCompatActivity() {
         val shirt = KeyPath("Shirt", "Group 5", "Fill 1")
         val leftArm = KeyPath("LeftArmWave", "LeftArm", "Group 6", "Fill 1")
         val rightArm = KeyPath("RightArm", "Group 6", "Fill 1")
+        val gradient3 = KeyPath("Shape Layer 1", "Ellipse 1", "Gradient Stroke 1")
 
-        binding.animationView.addValueCallback(shirt, LottieProperty.COLOR) { 0xff5a5f }
-        binding.animationView.addValueCallback(leftArm, LottieProperty.COLOR) { 0xff5a5f }
-        binding.animationView.addValueCallback(rightArm, LottieProperty.COLOR) { 0xff5a5f }
+        binding.animationView.addValueCallback(leftArm, LottieProperty.COLOR) { 0x55ff5a5f }
+        binding.animationView.addValueCallback(leftArm, LottieProperty.OPACITY) { 0 }
+        binding.animationView.addValueCallback(shirt, LottieProperty.COLOR) { 0x55ff5a5f }
+        binding.animationView.addValueCallback(shirt, LottieProperty.OPACITY) { 70 }
+        binding.animationView.addValueCallback(rightArm, LottieProperty.COLOR) { 0x55ff5a5f }
+        binding.animationView.addValueCallback(rightArm, LottieProperty.OPACITY) { 100 }
+
+        binding.animationView2.addValueCallback(gradient3, LottieProperty.OPACITY) { 10 }
     }
 }
